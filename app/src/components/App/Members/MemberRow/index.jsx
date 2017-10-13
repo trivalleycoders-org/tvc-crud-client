e8dcae4dee2e795bda1eb013f2040bae9a0ac094
Laura Silvey <lsilvey@gmail.com>
2017-10-13 13:40:52 -0700

    added table header

--- app/src/components/App/Members/MemberRow/index.jsx
+++ app/src/components/App/Members/MemberRow/index.jsx
@@ -3,10 +3,11 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import * as actionCreators from '../../../../store/actions'
+
 // import * as selectors from '../../../../store/selectors';
 import styles from './style.css'
 
-const MemberRow = ({ member_id, firstname, lastname, email, openMember, requestDeleteMember }) => {
+const MemberRow = ({ member_id, firstname, lastname, email, phone_number, openMember, requestDeleteMember }) => {
   const handleDeleteClick = () => {
     requestDeleteMember(member_id)
   }
@@ -14,10 +15,11 @@ const MemberRow = ({ member_id, firstname, lastname, email, openMember, requestD
   // { member_id, firstname, lastname, email }
   const renderItems = (
     <div className={styles.wrapper}>
-      <div className={styles.column}>{member_id}</div>
-      <div className={styles.column}>{firstname}</div>
-      <div className={styles.column}>{lastname}</div>
+
+      <div className={styles.column}>{firstname} {lastname}</div>
+      <div className={styles.column}>{phone_number}</div>
       <div className={styles.double_column}>{email}</div>
+      
       <button className={styles.menuButton} onClick={() => openMember(member_id)}>Edit</button>
       <button className={styles.menuButton} onClick={() => handleDeleteClick()}>Delete</button>
     </div>
--- app/src/components/App/Members/index.jsx
+++ app/src/components/App/Members/index.jsx
@@ -5,6 +5,7 @@ import { Redirect } from 'react-router-dom'
 import * as actionCreators from '../../../store/actions'
 import * as selectors from '../../../store/selectors'
 import MemberRow from './MemberRow'
+import { Table } from 'react-bootstrap'
 // import * as ku from '../../../lib/ke-utils'
 
 class Members extends Component {
@@ -33,8 +34,19 @@ class Members extends Component {
     return (
       <div>
         <h1>Members</h1>
-        {/* <Link to={`${match.url}/member-edit`}><button onClick={() => buttonClick()}>/member-edit</button></Link> */}
+        <Table>
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Text</th>
+              <th>Comments</th>
+            </tr>
+          </thead>
+        </Table> 
         {renderMembers}
+        {/* <Link to={`${match.url}/member-edit`}><button onClick={() => buttonClick()}>/member-edit</button></Link> */}
         <button onClick={createMember}>Add</button>
       </div>
     )
