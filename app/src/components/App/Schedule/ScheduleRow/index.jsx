0baacd1d6ea4f8403218bc9aa6f3976cbd7f2552
Jim X. Lin <miljinx@gmail.com>
2017-10-15 14:03:37 -0700

    Remove wrapper

--- app/src/components/App/Schedule/ScheduleRow/index.jsx
+++ app/src/components/App/Schedule/ScheduleRow/index.jsx
@@ -8,16 +8,14 @@ import styles from './style.css'
 
 const ScheduleRow = ({ member }) => {
   const renderItems = (
-    <div className={styles.wrapper}>
-      <div className={styles.row}>
-        <div className={styles.memberDetail}> {member.memberId} </div>
-        <div className={styles.memberDetail}> {member.sequence} </div>
-        <div className={styles.memberDetail}> {member.firstName} </div>
-        <div className={styles.memberDetail}> {member.lastName} </div>
-        <div className={styles.memberDetail}> {member.lastServedDate} </div>
-        <div className={styles.memberDetail}> {member.lastRoleId} </div>
-        <div className={styles.memberDetail}> {member.lastRoleName} </div>
-      </div>
+    <div className={styles.row}>
+      <div className={styles.memberDetail}> {member.memberId} </div>
+      <div className={styles.memberDetail}> {member.sequence} </div>
+      <div className={styles.memberDetail}> {member.firstName} </div>
+      <div className={styles.memberDetail}> {member.lastName} </div>
+      <div className={styles.memberDetail}> {member.lastServedDate} </div>
+      <div className={styles.memberDetail}> {member.lastRoleId} </div>
+      <div className={styles.memberDetail}> {member.lastRoleName} </div>
     </div>
   )
   return (
--- app/src/components/App/Schedule/index.jsx
+++ app/src/components/App/Schedule/index.jsx
@@ -42,7 +42,7 @@ class Schedule extends Component {
       let rRoles = roles.map((r) => {
         return {
           roleId: r.role_id,
-          roleName: r.title,
+          roleName: r.role_name,
           exempt: isExcluded(memberId, r.role_id)
         }
       })
@@ -75,16 +75,14 @@ class Schedule extends Component {
         <Link to='/editvolunteer'><button id='editScheduleBtn'>Edit</button></Link>
         <h1 className={styles.title}>Volunteer Schedule for [date] </h1>
         {/* <ScheduleRow /> */}
-        <div classname={styles.wrapper}>
-          <div className={styles.row}>
-            <div className={styles.memberDetail}>id</div>
-            <div className={styles.memberDetail}>seq</div>
-            <div className={styles.memberDetail}>first</div>
-            <div className={styles.memberDetail}>last</div>
-            <div className={styles.memberDetail}>lServeDate</div>
-            <div className={styles.memberDetail}>lRoleId</div>
-            <div className={styles.memberDetail}>lRoleName</div>
-          </div>
+        <div className={styles.row}>
+          <div className={styles.memberDetail}>id</div>
+          <div className={styles.memberDetail}>seq</div>
+          <div className={styles.memberDetail}>first</div>
+          <div className={styles.memberDetail}>last</div>
+          <div className={styles.memberDetail}>lServeDate</div>
+          <div className={styles.memberDetail}>lRoleId</div>
+          <div className={styles.memberDetail}>lRoleName</div>
         </div>
         {renderList}
       </div>
